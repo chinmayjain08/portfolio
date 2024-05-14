ed0a7cf1f20a44ffa7cc5114cf58e65b6f1e5842
Chinmay Jain <chinmayluhadiya62@gmail.com>
2024-05-14 15:47:46 +0530

    contact-responsive

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -15,36 +15,60 @@ function Contact() {
       <HorNavbar button={"ABOUT"} />
       <VerNavbar />
       <div className="main-contact">
-      <div className="form-data">
-        <div className="form-text">
-          <div className="contact-head">
-            Let's CONNECT <MdOutlineDoubleArrow />
-          </div>
-          <p className="contact-para">
-            Waiting to turn your thoughs to reality.
-          </p>
-          <p className="contact-para">
-            Phone: +91 9875760411
-            <p><a href="mailto:developerchinmay08@gmail.com" className="contact-mail">Send email</a></p>
-          </p>
+        <div className="form-data">
+          <div className="form-text">
+            <div className="contact-head">
+              Let's CONNECT <MdOutlineDoubleArrow />
+            </div>
+            <p className="contact-para">
+              Waiting to turn your thoughs to reality.
+            </p>
+            <p className="contact-para">
+              Phone: +91 9875760411
+              <p>
+                <a
+                  href="mailto:developerchinmay08@gmail.com"
+                  className="contact-mail"
+                >
+                  Send email
+                </a>
+              </p>
+            </p>
           </div>
           <section className="socials">
-            <a href='https://www.linkedin.com/in/chinmay-jain-493619147/' className='handle'><ImLinkedin/></a>
-            <a href='https://github.com/chinmayjain08/' className='handle'><ImGithub/></a>
-            <a href='https://www.instagram.com/iamchinmayjain/' className='handle'><ImInstagram/></a>
-            <a href='https://twitter.com/ChinmayJai2228/' className='handle'><FaXTwitter/></a>
-            <a href='https://wa.me/9875760411' className='handle'><ImWhatsapp/></a>
-        </section>
-          
+            <a
+              href="https://www.linkedin.com/in/chinmay-jain-493619147/"
+              className="handle"
+            >
+              <ImLinkedin />
+            </a>
+            <a href="https://github.com/chinmayjain08/" className="handle">
+              <ImGithub />
+            </a>
+            <a
+              href="https://www.instagram.com/iamchinmayjain/"
+              className="handle"
+            >
+              <ImInstagram />
+            </a>
+            <a href="https://twitter.com/ChinmayJai2228/" className="handle">
+              <FaXTwitter />
+            </a>
+            <a href="https://wa.me/9875760411" className="handle">
+              <ImWhatsapp />
+            </a>
+          </section>
         </div>
         <div className="form">
-          
           <form action="" className="contact-form">
             <input type="text" placeholder="Name" />
             <input type="tel" placeholder="Contact Number" />
             <input type="email" placeholder="Email Id" />
             <input type="text" placeholder="Message" />
-            <button className="contact-button" type="submit"> SUBMIT </button>
+            <button className="contact-button" type="submit">
+              {" "}
+              SUBMIT{" "}
+            </button>
           </form>
         </div>
       </div>
